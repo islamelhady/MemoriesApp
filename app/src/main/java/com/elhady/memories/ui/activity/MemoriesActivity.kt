05813f540c20bfbfaa15c5191c03f9ae5b9bbf8a
islamelhady <i.elhady44@gmail.com>
2021-03-03 11:02:00 +0200

    fix MemoriesActivity

--- app/src/main/java/com/elhady/memories/ui/activity/MemoriesActivity.kt
+++ app/src/main/java/com/elhady/memories/ui/activity/MemoriesActivity.kt
@@ -2,14 +2,35 @@ package com.elhady.memories.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.elhady.memories.R
+import androidx.lifecycle.ViewModelProvider
+import com.elhady.memories.database.MemoriesDatabase
+import com.elhady.memories.databinding.ActivityMemoriesBinding
+import com.elhady.memories.repository.MemoriesRepository
+import com.elhady.memories.utils.shortToast
+import com.elhady.memories.viewmodel.MemoriesViewModel
+import com.elhady.memories.viewmodel.MemoriesViewModelFactory
 
 /**
  * Created by islam elhady on 25-Feb-21.
  */
 class MemoriesActivity : AppCompatActivity() {
+
+    lateinit var memoriesViewModel: MemoriesViewModel
+    private lateinit var binding: ActivityMemoriesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_memories)
+        binding = ActivityMemoriesBinding.inflate(layoutInflater)
+        try {
+            setContentView(binding.root)
+            val memoriesRepository = MemoriesRepository(MemoriesDatabase(this))
+            val noteViewModelProviderFactory = MemoriesViewModelFactory(memoriesRepository)
+            memoriesViewModel = ViewModelProvider(
+                this,
+                noteViewModelProviderFactory
+            )[MemoriesViewModel::class.java]
+        } catch (e: Exception) {
+            shortToast("error occurred")
+        }
     }
 }
